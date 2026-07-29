bed4e89e636cbdaa667be625eac3bbed8a755ec1
Dhinesh Elumalai <babudhinesh17@gmail.com>
2026-07-29 10:38:58 +0530

    Add: max-water-container - submission-3

---
+++ Data Structures & Algorithms/max-water-container/submission-3.java
@@ -0,0 +1,51 @@
+class Solution {
+    public int maxArea(int[] heights) {
+        int maxArea = 0;
+
+        // [1,7,2,5,4,7,3,6]
+        // while(left<right){
+        //     int distance = right-left;
+        //     int height = Math.min(heights[left], heights[right]);
+        //     int currArea = height * distance;
+        //     if(currArea>maxArea){
+        //         maxArea= currArea;
+        //     }
+        //     //right--;
+        //     left++;
+        // }
+        //[1,7,2,5,12,3,500,500,7,8,4,7,3,6]
+        //BruteForce Approach
+        /* for (int i = 0; i < heights.length; i++) {
+            int left = i;
+            int right = heights.length - 1;
+            while (left < right) {
+                int distance = right - left;
+                int height = Math.min(heights[left], heights[right]);
+                int currArea = height * distance;
+                if (currArea > maxArea) {
+                    maxArea = currArea;
+                }
+                right--;
+            }
+        } */
+
+        // Efficient Approach:
+        // [1,7,2,5,4,7,3,6]
+        int left = 0;
+        int right = heights.length - 1;
+        while(left<right){
+            int distance = right-left;
+            int height = Math.min(heights[left], heights[right]);
+            int currArea = height * distance;
+            if(currArea>maxArea){
+                maxArea = currArea;
+            }
+            if(heights[left]>heights[right]){
+                right--;
+            } else{
+                left++;
+            }
+        }
+        return maxArea;
+    }
+}
